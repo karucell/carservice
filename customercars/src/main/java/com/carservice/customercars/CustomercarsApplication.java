a45a804e645eeb6764b5fb5f9d48d8afd1ca45fd
Marko Helle <marko.helle@karucell.com>
2018-09-29 16:17:45 +0300

    Customercars module

---
+++ customercars/src/main/java/com/carservice/customercars/CustomercarsApplication.java
@@ -0,0 +1,12 @@
+package com.carservice.customercars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomercarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomercarsApplication.class, args);
+	}
+}
