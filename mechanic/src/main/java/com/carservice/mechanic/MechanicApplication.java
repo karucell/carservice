c32fc295705718a2f1c37201891cc844a3a0d652
Marko Helle <marko.helle@karucell.com>
2018-09-29 16:18:24 +0300

    Mechanic module

---
+++ mechanic/src/main/java/com/carservice/mechanic/MechanicApplication.java
@@ -0,0 +1,12 @@
+package com.carservice.mechanic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechanicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MechanicApplication.class, args);
+    }
+}
